82350f397c4fbecc12dbbb3ce7d7ac8458d240d0
skiftare <74261401+Skiftare@users.noreply.github.com>
2022-09-10 10:47:30 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+file = 'table.xls'
+
+# Load spreadsheet
+xl = pd.ExcelFile(file)
+
+# Print the sheet names
+print(xl.sheet_names)
+
+# Load a sheet into a DataFrame by name: df1
+df1 = xl.parse('Sheet1')
